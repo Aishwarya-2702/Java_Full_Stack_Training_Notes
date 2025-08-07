1dca4b4c031bc7d0aa7d1ac7d629ae9dae65905b
Aishwarya-2702 <aishugeetha27@gmail.com>
2025-08-07 11:02:55 +0530

    hai

---
+++ hybernet_code/src/main/java/hyber/hybernet_code/App.java
@@ -0,0 +1,13 @@
+package hyber.hybernet_code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ hybernet_code/src/main/java/hyber/hybernet_code/hiber.java
@@ -0,0 +1,9 @@
+package hyber.hybernet_code;
+
+public class hiber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		}
+}
